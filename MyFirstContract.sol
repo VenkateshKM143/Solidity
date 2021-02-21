5437b0171465ef8bf5dc11e8360c66300bb86f1a
VenkateshKM143 <78034998+VenkateshKM143@users.noreply.github.com>
2021-02-21 20:13:36 +0530

    Create MyFirstContract.sol

---
+++ MyFirstContract.sol
@@ -0,0 +1,14 @@
+//Defining Versions
+pragma solidity ^0.6.0;
+
+//Can be considered as class
+ contract myFirstContract{
+     //State
+     
+     //function
+     //function funcName(arguments) AccessSpecifiers
+     
+  function greet() public view returns(string memory s) {
+      return "Hello world!";
+  }   
+ }
